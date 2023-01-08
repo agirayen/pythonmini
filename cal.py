8a1f85d0bca9491fd264d8ece582ffdddd9c0776
Agnita <ageetha2@pdx.edu>
2023-01-07 19:32:53 -0800

    print month and year

---
+++ cal.py
@@ -0,0 +1,10 @@
+
+
+import calendar
+print(calendar.month(1988, 11))
+print(calendar.month(1990, 9))
+print(calendar.month(2023, 9))
+print(calendar.month(2000,7))
+
+year = 2023
+print(calendar.calendar(year))
